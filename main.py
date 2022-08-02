7824ea4aa5d254ca499fa7e13e94d3627f25c178
Tyushnyakov Alexey <f95143@akado-ural.ru>
2022-08-02 08:31:34 +0500

    Задача №1

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # Домашнее задание «Открытие и чтение файла, запись в файл»:
-with open("recipe.txt") as file:
-    pass
+from pprint import pprint
+
+cook_book = {}
+
+with open('recipe.txt', encoding='utf-8') as file:
+    for recipe in file:
+        recipes = recipe.strip()
+        ingridients = int(file.readline(2))
+        recipe_list = []
+        for i in range(ingridients):
+            ingredient_name, quantity, measure = file.readline().strip().split(" | ")
+            recipe_list.append({"ingredient_name": ingredient_name, "quantity": quantity, "measure": measure})
+        cook_book[recipes] = recipe_list
+        file.readline()
+
+pprint(cook_book, sort_dicts=False, width=160)
