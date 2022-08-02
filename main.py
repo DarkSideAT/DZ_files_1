6ac40775a24548726c8e78fb69b945cf2551133d
Tyushnyakov Alexey <f95143@akado-ural.ru>
2022-08-02 08:53:15 +0500

    Задача №2

--- main.py
+++ main.py
@@ -15,3 +15,17 @@ with open('recipe.txt', encoding='utf-8') as file:
         file.readline()
 
 pprint(cook_book, sort_dicts=False, width=160)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ingredients = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            if ingredient['ingredient_name'] in ingredients:
+                ingredients[ingredient['ingredient_name']]['quantity']\
+                    = str(int(ingredients[ingredient['ingredient_name']]['quantity']) + int(ingredient['quantity'])* person_count)
+            else:
+                ingredients[ingredient['ingredient_name']] \
+                = {'quantity' : str(int(ingredient['quantity']) * person_count), 'measure' : ingredient['measure']}
+    return ingredients
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
