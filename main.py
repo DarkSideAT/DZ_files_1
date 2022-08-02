ec79887dec45fb1ac624f7ae0bf1cb79e68bdeb2
Tyushnyakov Alexey <f95143@akado-ural.ru>
2022-08-02 08:57:50 +0500

    Задача №3

--- main.py
+++ main.py
@@ -29,3 +29,15 @@ def get_shop_list_by_dishes(dishes, person_count):
     return ingredients
 
 print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
+with open("1.txt", encoding='utf-8') as f1, open("2.txt", encoding='utf-8') as f2, open("3.txt", encoding='utf-8') as f3:
+    file_1 = f1.readlines()
+    file_2 = f2.readlines()
+    file_3 = f3.readlines()
+    files_unsorted = {"1.txt": file_1, "2.txt": file_2, "3.txt": file_3}
+    files_sorted = sorted(files_unsorted, key=lambda key: len(files_unsorted[key]))
+    print(files_sorted)
+
+with open('result.txt', 'w', encoding='utf-8') as f:
+    for item in files_sorted:
+        f.write(f"{item}\n{len(files_unsorted[item])}\n {''.join(files_unsorted[item])}\n")
